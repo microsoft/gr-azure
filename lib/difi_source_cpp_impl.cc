75c651c08b25599e910ff5f92cb063286d745ccb
Marc L <marcll@vt.edu>
2022-06-09 11:29:31 -0400

    Fixed Steam ID of -1 Bug (#24)
    * fixed bug and created test
    
    * fixed the doc typo

--- lib/difi_source_cpp_impl.cc
+++ lib/difi_source_cpp_impl.cc
@@ -49,13 +49,13 @@ namespace gr {
     }
 
     template <class T>
-    typename difi_source_cpp<T>::sptr difi_source_cpp<T>::make(std::string ip_addr, uint32_t port, uint32_t stream_number, int bit_depth, int context_pkt_behavior)
+    typename difi_source_cpp<T>::sptr difi_source_cpp<T>::make(std::string ip_addr, uint32_t port, int stream_number, int bit_depth, int context_pkt_behavior)
     {
       return gnuradio::make_block_sptr<difi_source_cpp_impl<T>>(ip_addr, port, stream_number, bit_depth, context_pkt_behavior);
     }
 
     template <class T>
-    difi_source_cpp_impl<T>::difi_source_cpp_impl(std::string ip_addr, uint32_t port, uint32_t stream_number, int bit_depth, int context_pkt_behavior)
+    difi_source_cpp_impl<T>::difi_source_cpp_impl(std::string ip_addr, uint32_t port, int stream_number, int bit_depth, int context_pkt_behavior)
         : gr::sync_block("source_cpp", gr::io_signature::make(0, 0, 0),
                          gr::io_signature::make(1 /* min outputs */,
                                                 1 /*max outputs */,
